eb72e5d7d7f53811c1dcf6b3a614aa6055710114
SongJiAh <SongJiAh@users.noreply.github.com>
2019-10-19 10:54:21 -0400

    Create Test.js

---
+++ Test.js
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Ryan Big Brain.";
+}
+</script>
+</head>
+<body>
+
+<h1>A Web Page</h1>
+<p id="demo">Hidden</p>
+<button type="button" onclick="myFunction()">Click to reveal</button>
+
+</body>
+</html>
